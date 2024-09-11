75ee40527bc0991b1803a621c8fe30cb704129aa
drcm-ogawa-mitsunori <mitsunori.ogawa@drecom.co.jp>
2024-09-11 15:32:09 +0900

    build エラー無くす

--- src/components/WebAppViewer.tsx
+++ src/components/WebAppViewer.tsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
-import WebApp from '@twa-dev/sdk'
+// import WebApp from '@twa-dev/sdk'
 
 export const WebAppViewer = () => {
   const [webAppStrList, setWebAppStrList] = useState<Record<string, string | number>>({});
 
   const webAppStrListKeys = Object.keys(webAppStrList);
-  // if (webAppStrListKeys.length <= 0) {
-  //   try {
-  //     const newWebAppStrList: Record<string, string | number> = {};
-  //     newWebAppStrList.isExpanded = WebApp.isExpanded ? 'true' : 'false';
-  //     newWebAppStrList.viewportHeight = WebApp.viewportHeight;
-  //     newWebAppStrList.viewportStableHeight = WebApp.viewportStableHeight;
-  //     newWebAppStrList.platform = WebApp.platform;
-  //     newWebAppStrList.backgroundColor = WebApp.backgroundColor;
-  //     newWebAppStrList.isClosingConfirmationEnabled = WebApp.isClosingConfirmationEnabled ? 'true' : 'false';
-  //     // newWebAppStrList.themeParams = Object.keys(WebApp.themeParams).map(key => `${key}=${(WebApp.themeParams as any)[key]}`).join(','),
-  //     // newWebAppStrList.initDataUnsafe = Object.keys(WebApp.initDataUnsafe).map(key => `${key}=${(WebApp.initDataUnsafe as any)[key]}`).join(','),
-  //     newWebAppStrList.initData = WebApp.initData;
-  //     newWebAppStrList.colorScheme = WebApp.colorScheme;
-  //     setWebAppStrList(newWebAppStrList);
-  //   } catch (err) {
-  //     setWebAppStrList({
-  //       err: (err as any).message,
-  //     });
-  //   }
-  // }
+  if (webAppStrListKeys.length <= 0) {
+    try {
+      const newWebAppStrList: Record<string, string | number> = {};
+      newWebAppStrList.isExpanded = 'false';
+      // newWebAppStrList.isExpanded = WebApp.isExpanded ? 'true' : 'false';
+      // newWebAppStrList.viewportHeight = WebApp.viewportHeight;
+      // newWebAppStrList.viewportStableHeight = WebApp.viewportStableHeight;
+      // newWebAppStrList.platform = WebApp.platform;
+      // newWebAppStrList.backgroundColor = WebApp.backgroundColor;
+      // newWebAppStrList.isClosingConfirmationEnabled = WebApp.isClosingConfirmationEnabled ? 'true' : 'false';
+      // newWebAppStrList.themeParams = Object.keys(WebApp.themeParams).map(key => `${key}=${(WebApp.themeParams as any)[key]}`).join(','),
+      // newWebAppStrList.initDataUnsafe = Object.keys(WebApp.initDataUnsafe).map(key => `${key}=${(WebApp.initDataUnsafe as any)[key]}`).join(','),
+      // newWebAppStrList.initData = WebApp.initData;
+      // newWebAppStrList.colorScheme = WebApp.colorScheme;
+      setWebAppStrList(newWebAppStrList);
+    } catch (err) {
+      setWebAppStrList({
+        err: (err as any).message,
+      });
+    }
+  }
 
   return (
     <p>
