8823099eb9c59c089bcf35b5d770b5361cee56b3
drcm-ogawa-mitsunori <mitsunori.ogawa@drecom.co.jp>
2024-09-11 14:44:21 +0900

    エラーしてるっぽいので検知

--- src/components/WebAppViewer.tsx
+++ src/components/WebAppViewer.tsx
@@ -9,18 +9,24 @@ export const WebAppViewer = () => {
 
   const webAppStrListKeys = Object.keys(webAppStrList);
   if (webAppStrListKeys.length <= 0) {
-    const newWebAppStrList: Record<string, string | number> = {};
-    newWebAppStrList.isExpanded = WebApp.isExpanded ? 'true' : 'false';
-    newWebAppStrList.viewportHeight = WebApp.viewportHeight;
-    newWebAppStrList.viewportStableHeight = WebApp.viewportStableHeight;
-    newWebAppStrList.platform = WebApp.platform;
-    newWebAppStrList.backgroundColor = WebApp.backgroundColor;
-    newWebAppStrList.isClosingConfirmationEnabled = WebApp.isClosingConfirmationEnabled ? 'true' : 'false';
-    // newWebAppStrList.themeParams = Object.keys(WebApp.themeParams).map(key => `${key}=${(WebApp.themeParams as any)[key]}`).join(','),
-    // newWebAppStrList.initDataUnsafe = Object.keys(WebApp.initDataUnsafe).map(key => `${key}=${(WebApp.initDataUnsafe as any)[key]}`).join(','),
-    newWebAppStrList.initData = WebApp.initData;
-    newWebAppStrList.colorScheme = WebApp.colorScheme;
-    setWebAppStrList(newWebAppStrList);
+    try {
+      const newWebAppStrList: Record<string, string | number> = {};
+      newWebAppStrList.isExpanded = WebApp.isExpanded ? 'true' : 'false';
+      newWebAppStrList.viewportHeight = WebApp.viewportHeight;
+      newWebAppStrList.viewportStableHeight = WebApp.viewportStableHeight;
+      newWebAppStrList.platform = WebApp.platform;
+      newWebAppStrList.backgroundColor = WebApp.backgroundColor;
+      newWebAppStrList.isClosingConfirmationEnabled = WebApp.isClosingConfirmationEnabled ? 'true' : 'false';
+      // newWebAppStrList.themeParams = Object.keys(WebApp.themeParams).map(key => `${key}=${(WebApp.themeParams as any)[key]}`).join(','),
+      // newWebAppStrList.initDataUnsafe = Object.keys(WebApp.initDataUnsafe).map(key => `${key}=${(WebApp.initDataUnsafe as any)[key]}`).join(','),
+      newWebAppStrList.initData = WebApp.initData;
+      newWebAppStrList.colorScheme = WebApp.colorScheme;
+      setWebAppStrList(newWebAppStrList);
+    } catch (err) {
+      setWebAppStrList({
+        err: (err as any).message,
+      });
+    }
   }
 
   return (
